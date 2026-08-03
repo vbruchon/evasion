ae242e529a8bf0cfbe7ebc315dcb424649d555ff
vbruhon <vivian.bruchon@le-campus-numerique.fr>
2026-08-03 18:51:23 +0200

    fix: add Prisma singleton for Next.js hot reload

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,10 +1,26 @@
-import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../generated/prisma/client";
 
-const connectionString = `${process.env.DATABASE_URL}`;
+const connectionString = process.env.DATABASE_URL;
 
-const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
+if (!connectionString) {
+  throw new Error("DATABASE_URL is not defined");
+}
 
-export { prisma };
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+const adapter = new PrismaPg({
+  connectionString,
+});
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter,
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
