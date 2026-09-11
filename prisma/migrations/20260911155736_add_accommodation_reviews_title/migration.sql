3f67340c2f3ec1686bbd0542283b4d7641de0c4f
vbruhon <vivian.bruchon@le-campus-numerique.fr>
2026-09-11 17:58:35 +0200

    feat: add accommodation reviews content foundation

---
+++ prisma/migrations/20260911155736_add_accommodation_reviews_title/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "accommodations" ADD COLUMN     "reviewsTitle" TEXT NOT NULL DEFAULT 'Leurs moments, leurs mots';
---
+++ prisma/migrations/20260911164149_add_accommodation_reviews_description/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "accommodations" ADD COLUMN     "reviewsDescription" TEXT NOT NULL DEFAULT 'Découvrez les impressions laissées par les voyageurs après leur séjour.';
